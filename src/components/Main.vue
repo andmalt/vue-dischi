c2d59cca9e4d1a3b7353389e2a8a581780989448
andmalt <andrea.maltempi@gmail.com>
2021-09-29 16:09:55 +0200

    removed component helloword and inserted main and general.scss

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
